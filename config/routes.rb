a93a33d630c010dee78dc3c753a5dc6fe722a551
YukaNakamura <242622452+nakamurapaysyanisland@users.noreply.github.com>
2025-12-21 12:09:35 +0000

    [Add]app2-17

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 devise_for :users
 root to: "homes#top"
 
-resources :users, only: [:show, :edit]
+resources :users, only: [:show, :edit, :update]
 resources :post_images, only: [:new, :create, :index, :show, :destroy]
 
 get 'homes/about', to: 'homes#about', as: 'about'
