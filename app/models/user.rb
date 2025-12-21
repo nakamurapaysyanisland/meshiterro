fa892f60f67eff146d60ba54cc8fd9a6056318d1
YukaNakamura <242622452+nakamurapaysyanisland@users.noreply.github.com>
2025-12-21 09:00:50 +0000

    [Add]app2-14

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :articles, dependent: :destroy
+  has_many :post_images, dependent: :destroy
 
   has_one_attached :profile_image
 
